a21da2c9b132e2bb3ffce77f45492c6f48d54ee8
abhim-0912 <abhi.garg.0912@gmail.com>
2025-03-14 23:05:10 +0530

    Created the Signup page

---
+++ src/public/signup.js
@@ -0,0 +1,19 @@
+document.getElementById("signupForm").addEventListener("submit",async function(event) {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const response = await fetch('/user/signup', {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({name,email,password})
+    });
+
+    const result = await response.json();
+    if(response.ok){
+        alert('Signup Successful');
+    } else {
+        alert(result.message || "Signup failed.");
+    }
+});
